972dc6c38c42f18a10277ad8de166f7d0c525a97
apatha <apatha@NIST-MAC-L-0051s-MacBook-Pro.local>
2016-09-26 12:05:18 -0700

    [Alok]:adding routingfor remaing pages

--- src/main/webapp/angularJs/vResume.js
+++ src/main/webapp/angularJs/vResume.js
@@ -17,7 +17,23 @@
         }).state('main.profile', {
 	            url: '/profile',
 	            templateUrl: 'partials/profile/profile.html'
-	    });
+	    }).state('main.openings', {
+            url: '/allUsers',
+            templateUrl: 'partials/openings.html'
+        }).state('main.templates', {
+            url: '/allUsers',
+            templateUrl: 'partials/templates.html'
+        }).state('main.myJobsConsultancy', {
+            url: '/allUsers',
+            templateUrl: 'partials/myJobsConsultancy.html'
+        }).state('main.myJobs', {
+            url: '/allUsers',
+            templateUrl: 'partials/myJobs.html'
+        }).state('main.allUsers', {
+            url: '/allUsers',
+            templateUrl: 'partials/allUsers.html'
+        });
+	    
 	    
 	    $urlRouterProvider.otherwise('/');
 });
