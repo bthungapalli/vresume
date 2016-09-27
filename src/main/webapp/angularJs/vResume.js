ff6dbf493c1b5332732551525bbb35406cc84e27
apatha <apatha@NIST-MAC-L-0051s-MacBook-Pro.local>
2016-09-27 08:16:02 -0700

    [Alok]:adding templates

--- src/main/webapp/angularJs/vResume.js
+++ src/main/webapp/angularJs/vResume.js
@@ -11,6 +11,10 @@
 	    $stateProvider.state('login', {
             url: '/login',
             templateUrl: 'partials/login/login.html'
+        }).state('login.loginTemplate', {
+            templateUrl: 'partials/login/loginTemplate.html'
+        }).state('login.signupTemplate', {
+            templateUrl: 'partials/login/signupTemplate.html'
         }).state('main', {
             url: '/main',
             templateUrl: 'partials/main/main.html'
@@ -18,22 +22,42 @@
 	            url: '/profile',
 	            templateUrl: 'partials/profile/profile.html'
 	    }).state('main.openings', {
-            url: '/allUsers',
+            url: '/openings',
             templateUrl: 'partials/openings.html'
         }).state('main.templates', {
-            url: '/allUsers',
+            url: '/templates',
             templateUrl: 'partials/templates.html'
         }).state('main.myJobsConsultancy', {
             url: '/allUsers',
             templateUrl: 'partials/myJobsConsultancy.html'
         }).state('main.myJobs', {
-            url: '/allUsers',
+            url: '/myJobs',
             templateUrl: 'partials/myJobs.html'
         }).state('main.allUsers', {
             url: '/allUsers',
             templateUrl: 'partials/allUsers.html'
+        }).state('main.mySubmissions', {
+            url: '/mySubmissions',
+            templateUrl: 'partials/mySubmissions.html'
+        }).state('main.viewResume', {
+            url: '/viewResume',
+            templateUrl: 'partials/viewResume.html'
+        }).state('main.newUser', {
+            url: '/newUser',
+            templateUrl: 'partials/newUser.html'
+        }).state('main.viewSubmission', {
+            url: '/viewSubmission',
+            templateUrl: 'partials/viewSubmission.html'
+        }).state('main.newTemplate', {
+            url: '/newTemplate',
+            templateUrl: 'partials/newTemplate.html'
+        }).state('main.showTemplate', {
+            url: '/showTemplate',
+            templateUrl: 'partials/showTemplate.html'
+        }).state('main.editTemplate', {
+            url: '/editTemplate',
+            templateUrl: 'partials/editTemplate.html'
         });
-	    
 	    
 	    $urlRouterProvider.otherwise('/');
 });
