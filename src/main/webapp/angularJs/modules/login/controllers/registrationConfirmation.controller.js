eee90fbeaf659b848f00392d99753acee4543284
apatha <apatha@NIST-MAC-L-0051s-MacBook-Pro.local>
2016-10-29 15:09:10 +0530

    [ALok]: register confirmation validation

--- src/main/webapp/angularJs/modules/login/controllers/registrationConfirmation.controller.js
+++ src/main/webapp/angularJs/modules/login/controllers/registrationConfirmation.controller.js
@@ -1,17 +1,19 @@
 (function(){
 	
 	function registrationConfirmationController($scope,$state,loginFactory,$location){
+		
+		
 				loginFactory.registrationConfirmation($location.search().token).then(function(response){
 					if(response.success!==undefined){
 						$scope.success=response.success;
-					}else{
-						$scope.error=response.error;
 					}
 				}).catch(function(error){
-					
+					$scope.error=error.failed;
 	            });
-		
+			
 	};
+	
+	
 	
 	registrationConfirmationController.$inject=['$scope','$state','loginFactory','$location'];
 	
