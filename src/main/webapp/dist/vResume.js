a9dd5b4de3cf32084610b964e59644dcd9586acd
apatha <apatha@NIST-MAC-L-0051s-MacBook-Pro.local>
2016-11-15 19:55:29 -0800

    [Alok]: changes for confirmation instructions

--- src/main/webapp/dist/vResume.js
+++ src/main/webapp/dist/vResume.js
@@ -312,22 +312,22 @@
 
 (function(){
 	
-	function registrationConfirmationController($scope,$state,loginFactory,$location){
-		
-		
+	function registrationConfirmationController($scope,$state,loginFactory,$location,$loading){
+		        $loading.start('register');
 				loginFactory.registrationConfirmation($location.search().token).then(function(response){
 					if(response.success!==undefined){
 						$scope.success=response.success;
 					}
+					$loading.finish('register');
 				}).catch(function(error){
 					$scope.error=error.failed;
+					$loading.finish('register');
 	            });
-			
 	};
 	
 	
 	
-	registrationConfirmationController.$inject=['$scope','$state','loginFactory','$location'];
+	registrationConfirmationController.$inject=['$scope','$state','loginFactory','$location','$loading'];
 	
 	angular.module('vResume.login').controller("registrationConfirmationController",registrationConfirmationController);
 	
